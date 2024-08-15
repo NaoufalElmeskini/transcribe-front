18d924d9a9b9f3e1d646486fda17145f235b10ef
Naoufal EM <naoufal.elmeskini@kaibee.fr>
2024-08-15 08:27:13 +0200

    ajouter bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Importer le CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Importer le JS de Bootstrap (si nécessaire)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createApp(App).mount('#app')
